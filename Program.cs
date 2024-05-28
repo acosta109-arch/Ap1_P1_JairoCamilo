5664cb1f59e6420ed42f05100a2b110b59781e83
jairo camilo acosta <jairo_camilo@ucne.edu.do>
2024-05-27 20:52:55 -0400

    El proyecto ha sido creado.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using Ap1_P1_JairoCamilo;
 using Ap1_P1_JairoCamilo.Components;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -5,6 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddBlazorBootstrap();
 
 var app = builder.Build();
 
