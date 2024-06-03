978178669f9b7fabe0ad48c2da249162c7948c13
jairo camilo acosta <jairo_camilo@ucne.edu.do>
2024-06-03 18:52:08 -0400

    Creando el contexto.

---
+++ DAL/Contexto.cs
@@ -0,0 +1,14 @@
+﻿using Ap1_P1_JairoCamilo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ap1_P1_JairoCamilo.DAL;
+
+public class Contexto : DbContext
+{
+    public Contexto(DbContextOptions<Contexto> options) : base(options)
+    {
+        
+    }
+
+    public DbSet<Articulos> Articulos { get; set; }
+}
