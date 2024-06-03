e09a2ba69c2b5a4adee240df08cc240ac443c214
jairo camilo acosta <jairo_camilo@ucne.edu.do>
2024-06-03 18:30:50 -0400

    Creando el modelo Articulos.

---
+++ Models/Articulos.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ap1_P1_JairoCamilo.Models;
+
+public class Articulos
+{
+    //costo por % de ganancia entre 100 mas costo
+ 
+    [Key]
+    public int ArticuloId { get; set; }
+
+    [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Favor de digitar una descripción.")]
+    public string Descripcion { get; set; }
+
+    [Range(1,200000, ErrorMessage = "Favor de digitar el costo del articulo.")]
+    public decimal Costo { get; set; }
+
+    [Range(1, 200000, ErrorMessage = "Favor de digitar la ganancia del articulo.")]
+    public decimal Ganancia { get; set; }
+
+    [Range(1,200000, ErrorMessage = "Favor de digitar el precio.")]
+    public decimal Precio { get; set; }
+
+}
