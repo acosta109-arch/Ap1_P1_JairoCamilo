08a3c621988625818f67fd3c464889970b413821
jairo camilo acosta <jairo_camilo@ucne.edu.do>
2024-06-03 20:32:22 -0400

    Nueva migración.

---
+++ Migrations/20240603235816_Articulo.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ap1_P1_JairoCamilo.Migrations
+{
+    /// <inheritdoc />
+    public partial class Articulo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
